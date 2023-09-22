af2bfa2e508ea18e3c80785789a106f77f3f7415
naimish-developer <naimish-developer@example.com>
2023-09-22 19:04:06 +0530

    chang on sw file

--- public/sw.js
+++ public/sw.js
@@ -10,13 +10,22 @@ this.addEventListener("install", (event) => {
       .then((catche) =>
         catche.addAll([
           "/static/js/bundle.js",
+          "/static",
+          "/static/",
+          "/static/js",
+          "/static/js/S",
           "/static/js/main.be293e38.js",
+          "/static/js/index.js",
+          "/static/js/App.js",
           "/static/css/main.300b1910.css",
+          "/static/css/index.css",
           "/ogo192.png",
           "/index.html",
+          "/index",
           "/",
+          "main--lively-jelly-1f692c.netlify.app"
         ])
-      )
+      ).then((data)=>{console.log(data);})
       .catch((error) => console.log("Catch Store  Faild", error))
   );
 });
