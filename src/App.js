ea98d3a9f1ad19802365719f0636fe5a3b418501
naimish-developer <140482119+naimish-developer@users.noreply.github.com>
2023-09-22 17:50:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -121,12 +121,11 @@ setApiData(JSON.parse(localStorage.getItem("LocalApiData")))
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?
-            <a
-              href="#"
+            <button
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Start a 14 day free trial
-            </a>
+            </button>
           </p>
         </div>
 
